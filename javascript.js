ca013d266a547c56134dfe532ab0953a7e44823d
Matt Mont <mattmont415@gmail.com>
2020-11-23 11:11:37 -0800

    Initial Commit - Working Html and Css

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+
+//Math Operations
+function add(a, b) {
+  return parseFloat(a) + parseFloat(b);
+}
+
+function subtract(a, b) {
+  return parseFloat(a) - parseFloat(b);
+}
+
+function multiply(a, b) {
+  return parseFloat(a) * parseFloat(b);
+}
